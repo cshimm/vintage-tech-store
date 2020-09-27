8827728594da855676122925ef448b410a273fa3
Cameron Shimmin <cameronshimmin@Camerons-Mac-mini.local>
2020-09-26 22:45:48 -0700

    setup

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,17 @@
-import React from "react";
+import React from 'react';
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos culpa
+        obcaecati rerum necessitatibus libero aut repellendus error nostrum illo
+        recusandae et quasi provident officiis, perferendis eos! Temporibus
+        animi sed dolorum, rerum optio illo aperiam consequatur iure est maiores
+        similique, dolore itaque expedita. Sed fuga ducimus, perspiciatis fugit
+        facilis iure quisquam labore a quasi enim hic?
+      </p>
+    </section>
+  );
 }
