3e556157853b8c408e9f2b66bb8956a77f9dd69e
Cameron Shimmin <cameronshimmin@Camerons-Mac-mini.local>
2020-10-03 20:57:38 -0700

    refacto4

--- src/components/Products/ProductList.js
+++ src/components/Products/ProductList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Product from './Product';
 
-export default function ProductList({ title, products }) {
+export default function ProductList({ products }) {
   // console.log(products);
+
   return (
     <section className='section'>
-      <h2 className='section-title'>{products.title}</h2>
+      <h2 className='section-title'>Products</h2>
       <div className='products-center'>
         {products.map((item) => {
           return <Product key={item.key} {...item} />;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
           our products
         </Link>
       </Hero>
-      <FeaturedProducts />
+      {/* <FeaturedProducts /> */}
     </>
   );
 }
