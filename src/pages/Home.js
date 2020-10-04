fdbd25950c710abeefa1cadeb8d4b3101c7b1172
Cameron Shimmin <cameronshimmin@Camerons-Mac-mini.local>
2020-10-03 20:58:26 -0700

    ref5

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import { Link } from 'react-router-dom';
-import FeaturedProducts from '../components/Products/FeaturedProducts';
+// import FeaturedProducts from '../components/Products/FeaturedProducts';
 export default function Home() {
   return (
     <>
